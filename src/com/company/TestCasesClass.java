ad13bdb6e0303caf1a81602751cd6b50ebab920d
Sherlocked1 <mohamadsayed@git.com>
2018-09-15 20:23:31 +0200

    test Method name using @Rule

--- src/com/company/TestCasesClass.java
+++ src/com/company/TestCasesClass.java
@@ -1,9 +1,20 @@
 package com.company;
 
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestName;
+
 import static org.junit.Assert.*;
 
 public class TestCasesClass {
 
-    
+    @Rule
+    public TestName testName = new TestName();
+
+    @Test
+    public void testMethodName()
+    {
+        assertEquals("testMethodName",testName.getMethodName());
+    }
 
 }
